a98a497479aad017093942a313b68bc9212fdca1
Wayaer <wayaer@foxmail.com>
2019-12-13 23:49:40 +0800

    updata code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,39 +1,37 @@
-import 'dart:convert';
-
+import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:picture_picker/picture_picker.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(home: MyApp()));
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Picture Picker Plugin example app'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Center(),
-            RaisedButton(
-                child: Text('打开相册'),
-                onPressed: () {
-                  print('打开相册');
-                  openSelect();
-                }),
-            Container(height: 20),
-            RaisedButton(
-                child: Text('打开相机'),
-                onPressed: () {
-                  print('打开相机');
-                  openCamera();
-                })
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Picture Picker Plugin example app'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(),
+          RaisedButton(
+              child: Text('打开相册'),
+              onPressed: () {
+                print('打开相册');
+                openSelect();
+              }),
+          Container(height: 20),
+          RaisedButton(
+              child: Text('打开相机'),
+              onPressed: () {
+                print('打开相机');
+                openCamera();
+              }),
+       
+        ],
       ),
     );
   }
